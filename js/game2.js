998546e53b6f7341f5ca1594be2c6b9a232aa569
danielgadot <danielgadot@gmail.com>
2016-06-13 23:03:26 +0300

    game 2 added win condition and drag and drop

---
+++ js/game2.js
@@ -0,0 +1,35 @@
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    isWin();
+}
+
+function isWin(){
+    var a = document.querySelector('.winNums');
+    var li1 = document.querySelector('#li1');
+    var li2 = document.querySelector('#li2');
+    var t1 = document.querySelector('#drag1');
+    var t2 = document.querySelector('#drag2');
+    var x1 = parseInt(li1.innerText);
+    var x2 = parseInt(li2.innerText);
+    
+    
+    console.log('x1: ',x1);
+    console.log('li2: ',li2.innerText);
+    console.log('t1: ',t1.innerHTML);
+    console.log('t2: ',t2.innerHTML);
+    
+    if (x1 === 3 && x2 === 7)    alert('WIN');
+    
+}
+
+isWin();
