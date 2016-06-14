6abf0886c915fdc050d30a1ef671930c4bab7014
zafrirdotan <zafrir.dotan@gmail.com>
2016-06-14 13:38:34 +0300

    started game 1

--- js/game2.js
+++ js/game2.js
@@ -1,17 +1,42 @@
-function allowDrop(ev) {
-    ev.preventDefault();
-}
+// function allowDrop(ev) {
+//     ev.preventDefault();
+// }
 
-function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-}
 
-function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-    isWin();
-}
+
+$(document).ready(function(){
+    
+$('#drag1').draggable({
+   revert: 'invalid'
+});
+$('#drag2').draggable({
+   revert: 'invalid'    
+});
+$('#li1').droppable({
+    drop: function(e,ui){
+        $(ui.draggable).css('position', 'static').appendTo(this);
+    }
+});
+$('#li2').droppable({
+    drop: function(e,ui){
+        $(ui.draggable).css('position','static').appendTo(this);
+    }
+});
+});
+    
+    
+    
+    
+// function drag(ev) {
+//     ev.dataTransfer.setData("text", ev.target.id);
+// }
+
+// function drop(ev) {
+//     ev.preventDefault();
+//     var data = ev.dataTransfer.getData("text");
+//     ev.target.appendChild(document.getElementById(data));
+//     isWin();
+// }
 
 function isWin(){
     var a = document.querySelector('.winNums');
@@ -29,7 +54,8 @@ function isWin(){
     console.log('t2: ',t2.innerHTML);
     
     if (x1 === 3 && x2 === 7)    alert('WIN');
+    else                         alert('NOT');
     
 }
-
-isWin();
+// setInterval(function(){isWin();},1000);
+// isWin();
