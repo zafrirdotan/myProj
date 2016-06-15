acc8d55968b405277048b25ee62b6550492d2ca1
danielgadot <danielgadot@gmail.com>
2016-06-15 09:23:10 +0300

    game4 basic js

---
+++ js/game4.js
@@ -0,0 +1,20 @@
+$('#choise1').click(function(){
+    console.log('clicked 1');
+    
+})
+$('#choise2').click(function(){
+    console.log('clicked 2');
+    
+})
+$('#choise3').click(function(){
+    console.log('clicked 3');
+    
+})
+$('#choise4').click(function(){
+    console.log('clicked 4');
+    
+})
+$('#choise5').click(function(){
+    console.log('clicked 5');
+    
+})
