a84e4adbfc5d42752fded736928ac98b3f354b93
danielgadot <danielgadot@gmail.com>
2016-06-15 10:57:00 +0300

    game 4 logic,game 2 media query added

--- js/game4.js
+++ js/game4.js
@@ -1,20 +1,47 @@
-$('#choise1').click(function(){
-    console.log('clicked 1');
+var numOfSheeps;
+
+renderChoises();
+renderSheeps();
+
+
+$('#choise1,#choise2,#choise3,#choise4,#choise5').click(function(){
+    $('body').click(function( event ) {
+        var str = event.target.id;
+        str = '#' + str;
+        var num = parseInt($(str).text().match(/\d+/)[0], 10); 
+        if (num === numOfSheeps) {
+            console.log('WIN');
+            // gChals[3].isSolved = true;
+            
+        }
+        else {
+            console.log('NOT');
+            
+        }                     
     
-})
-$('#choise2').click(function(){
-    console.log('clicked 2');
-    
-})
-$('#choise3').click(function(){
-    console.log('clicked 3');
-    
-})
-$('#choise4').click(function(){
-    console.log('clicked 4');
-    
-})
-$('#choise5').click(function(){
-    console.log('clicked 5');
-    
-})
+    });
+});
+
+
+function renderSheeps(){
+    var rand = parseInt(Math.random()*5) + 1;
+    numOfSheeps = rand;
+    var elContainer = document.querySelector('.upCont4');
+    var strHTML ='';
+    for (var i = 0;i < rand ; i++){
+        strHTML += '<img src="img/sheep-game4.png" alt="">' 
+    }
+    elContainer.innerHTML = strHTML;
+}
+
+function renderChoises(){
+    // var rand = parseInt(Math.random()*5) + 1;
+    // numOfSheeps = rand;
+    var elContainer = document.querySelector('.downCont4');
+    var strHTML ='<ul>';
+    for (var i = 1;i < 6 ; i++){
+        strHTML += ' <li id="choise'+ i +'">'+ i + '</li>';
+    }
+    strHTML +='</ul>';
+    elContainer.innerHTML = strHTML;
+}
